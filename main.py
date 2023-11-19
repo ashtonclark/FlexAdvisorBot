47f3997ec9517949ebd57f9a23027a78cf939d70
ashtonclark <147894295+ashtonclark@users.noreply.github.com>
2023-11-18 19:08:48 -0800

    Added Bot

---
+++ main.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+import outputs
+import random
+import os
+
+workout_plans = {
+    "lose_weight": [
+        outputs.cardioPlan1,
+        outputs.cardioPlan2,
+        outputs.cardioPlan3
+    ],
+    "strength": [
+        outputs.strengthPlan1,
+        outputs.strengthPlan2,
+        outputs.strengthPlan3
+    ]
+}
+
+intents = discord.Intents.all()
+intents.typing = False
+intents.presences = False
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.command()
+async def workout(ctx, goal: str):
+    if goal.lower() == 'lose_weight':
+        workout = random.choice(workout_plans['lose_weight'])
+        await ctx.send(f'For weight loss, your workout routine is:\n{workout}')
+    elif goal.lower() == 'strength':
+        workout = random.choice(workout_plans['strength'])
+        await ctx.send(f'For strength, your workout routine is:\n{workout}')
+    elif goal.lower() == 'hello':
+        await ctx.send('Hello!')
+    elif goal.lower() == 'coleman':
+        await ctx.send('LIGHTWEIGHT')
+    else:
+        await ctx.send("Please specify either 'lose_weight' or 'strength' as your goal.")
+
+bot.run('MTE3NTUwNzQ3ODI5Mzc4NjY3NQ.G798qC.4_zxIzW300-AXoKOMiujKnrC4K8n2LKimOrQRY')
---
+++ outputs.py
@@ -0,0 +1,182 @@
+cardioPlan1 = """
+Weekly Routine: Cardio Emphasis
+
+# Day 1: Cardio and Hypertrophy
+1. 30 minutes of brisk walking or jogging
+2. Bodyweight squats: 3 sets of 15 reps
+3. Push-ups: 3 sets of 12 reps
+4. Plank: 3 sets, hold for 30 seconds each
+
+# Day 2: Active Recovery
+* Light activities such as walking or yoga
+
+# Day 3: High-Intensity Interval Training (HIIT)
+1. Warm-up: 5 minutes of light jogging in place
+2. HIIT Circuit (Repeat 3 times):
+* Jumping jacks: 1 minute
+* Burpees: 1 minute
+* Mountain climbers: 1 minute
+* Rest: 1 minute
+
+# Day 4: Rest
+
+# Day 5: Cardio and Hypertrophy
+1. 30 minutes of cycling or swimming
+2. Lunges: 3 sets of 12 reps per leg
+3. Dumbbell rows: 3 sets of 12 reps per arm
+4. Plank with shoulder taps: 3 sets, 15 taps per shoulder
+
+# Day 6: Active Recovery
+* Light activities and stretching
+
+# Day 7: Rest
+"""
+strengthPlan1 = """
+# Weekly Routine: Strength Training Focus
+
+# Day 1: Full Body Strength (Higher Weight, Lower Reps)
+1.Squats: 4 sets of 10 reps
+2.Bench press or push-ups: 3 sets of 12 reps
+3.Bent-over rows: 4 sets of 10 reps
+4.Plank: 3 sets, hold for 45 seconds each
+
+# Day 2: Cardio
+* 30-40 minutes of moderate-intensity cardio (e.g., brisk walking, cycling)
+
+# Day 3: Rest or Light Activity
+
+# Day 4: Full Body Strength (Higher Weight, Lower Reps)
+1. Deadlifts: 3 sets of 8 reps
+2. Overhead press or dumbbell shoulder press: 3 sets of 12 reps
+3. Pull-ups or lat pulldowns: 3 sets of 10 reps
+4. Russian twists: 3 sets of 20 twists
+
+# Day 5: Cardio
+* 30-40 minutes of moderate-intensity cardio
+
+# Day 6: Rest or Light Activity
+
+# Day 7: Rest
+"""
+cardioPlan2 = """
+# Cardio Routine
+
+# Day 1: Moderate-Intensity Cardio
+* 40 minutes of brisk walking or cycling
+
+# Day 2: Active Recovery
+* Yoga or stretching exercises
+
+# Day 3: Interval Training
+1. Warm-up: 10 minutes of light jogging in place
+2. Interval Circuit (Repeat 4 times):
+* Sprinting: 30 seconds
+* Walking or slow jogging: 1 minute
+* Jump rope: 2 minutes
+* Rest: 1 minute
+
+# Day 4: Rest
+
+# Day 5: Cardio and Bodyweight Exercises
+30 minutes of swimming or cycling
+Bodyweight squats: 3 sets of 15 reps
+Push-ups: 3 sets of 12 reps
+Plank: 3 sets, hold for 30 seconds each
+
+# Day 6: Long-Distance Cardio
+60 minutes of brisk walking, jogging, or cycling
+
+# Day 7: Rest
+"""
+cardioPlan3 = """
+# Cardio Routine
+
+# Day 1: Steady-State Cardio
+* 45 minutes of brisk walking or light jogging
+
+# Day 2: Active Recovery
+* Pilates or low-impact aerobics
+
+# Day 3: Pyramid Intervals
+1. Warm-up: 10 minutes of light jogging in place
+2. Pyramid Intervals (Repeat 3 times):
+* Sprinting: 20 seconds
+* Jogging: 40 seconds
+* High knees: 30 seconds
+* Rest: 1 minute
+
+# Day 4: Rest
+
+# Day 5: Cardio and Total Body Workout
+1. 40 minutes of cycling
+2. Jump squats: 3 sets of 15 reps
+3. Burpees: 3 sets of 12 reps
+4. Plank with alternating leg lifts: 3 sets, 20 seconds per leg
+
+# Day 6: Trail Run
+* 45 minutes of trail running or hiking
+
+# Day 7: Rest
+"""
+strengthPlan2 = """
+# Strength Routine
+
+# Day 1: Lower Body Strength
+1. Barbell Back Squats: 5 sets of 5 reps
+2. Romanian Deadlifts: 4 sets of 8 reps
+3. Leg Press: 3 sets of 10 reps
+4. Standing Calf Raises: 3 sets of 15 reps
+
+# Day 2: Active Recovery
+* Walking or light jogging for 30 minutes
+
+# Day 3: Upper Body Strength
+1. Barbell Bench Press: 5 sets of 5 reps
+2. Bent-over Rows: 4 sets of 8 reps
+3. Overhead Press: 3 sets of 10 reps
+4. Pull-ups: 3 sets to failure
+
+# Day 4: Rest or Light Activity
+
+# Day 5: Deadlift Focus
+1. Deadlifts: 5 sets of 5 reps
+2. Romanian Deadlifts: 3 sets of 8 reps
+3. Barbell Shrugs: 3 sets of 12 reps
+4. Plank: 3 sets, hold for 60 seconds each
+
+# Day 6: Cardio
+* 20 minutes of high-intensity interval training on a rowing machine or stationary bike
+
+# Day 7: Rest
+"""
+strengthPlan3 = """
+# Strength Routine
+
+# Day 1: Lower Body Power
+1. Power Cleans: 5 sets of 3 reps
+2. Barbell Back Squats: 4 sets of 8 reps
+3. Lunges: 3 sets of 12 reps per leg
+4. Seated Calf Raises: 3 sets of 15 reps
+
+#Day 2: Active Recovery
+* Swimming or light cycling for 30 minutes
+
+# Day 3: Upper Body Strength
+1. Weighted Dips: 4 sets of 6 reps
+2. Barbell Rows: 5 sets of 5 reps
+3. Incline Bench Press: 3 sets of 10 reps
+4. Face Pulls: 3 sets of 15 reps
+
+# Day 4: Rest or Light Activity
+
+# Day 5: Deadlift Focus
+1. Sumo Deadlifts: 5 sets of 5 reps
+2. Bent-over Rows: 4 sets of 8 reps
+3. Hammer Curls: 3 sets of 12 reps per arm
+4. Plank: 3 sets, hold for 60 seconds each
+
+# Day 6: Cardio
+* 20 minutes of rowing or cycling at a moderate pace
+
+# Day 7: Rest
+"""
